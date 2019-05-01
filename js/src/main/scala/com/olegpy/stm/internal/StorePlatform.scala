4355048431504de6c941a19772362e6379ff0fd5
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2019-05-01 11:50:28 +0300

    feat: native JS Maps instead of scala ones

--- js/src/main/scala/com/olegpy/stm/internal/StorePlatform.scala
+++ js/src/main/scala/com/olegpy/stm/internal/StorePlatform.scala
@@ -1,22 +1,26 @@
 package com.olegpy.stm.internal
 
-import scala.collection.mutable
 import scala.scalajs.js
 
 
 trait StorePlatform {
   def forPlatform(): Store = new Store {
+    import scala.scalajs.js.DynamicImplicits._
+    @inline implicit def any2JSAny(x: Any): js.Any = x.asInstanceOf[js.Any]
     private[this] val committed =
       js.Dynamic.newInstance(js.Dynamic.global.WeakMap)()
 
     class Journal extends Store.Journal {
-      private[this] def toJS(any: Any): js.Any = any.asInstanceOf[js.Any]
-      private[this] val uncommitted = mutable.AnyRefMap[AnyRef, Any]()
+      val uncommitted : js.Dynamic =
+        js.Dynamic.newInstance(js.Dynamic.global.Map)()
+
       def read(k: AnyRef): Any =
-        uncommitted.getOrElse(k, committed.get(toJS(k)))
-      def update(k: AnyRef, v: Any): Unit = uncommitted.update(k, v)
-      def commitAll(): Unit = uncommitted.foreach {
-        case (k, v) => committed.set(toJS(k), toJS(v))
+        if (uncommitted.has(k)) uncommitted.get(k)
+        else committed.get(k)
+
+      def update(k: AnyRef, v: Any): Unit = {
+        uncommitted.set(k, v)
+        ()
       }
     }
 
@@ -25,7 +29,13 @@ trait StorePlatform {
     def transact[A](f: => A): A = {
       theLog = new Journal
       val result = f
-      theLog.commitAll()
+      val it = theLog.uncommitted.asInstanceOf[js.Iterable[js.Array[js.Any]]].jsIterator()
+      var entry = it.next()
+      while (!entry.done) {
+        val arr = entry.value
+        committed.set(arr(0), arr(1))
+        entry = it.next()
+      }
       theLog = null
       result
     }
